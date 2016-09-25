5415b2aa72e218ef3b52fb5e41f86b6c62614f65
Fernando Pauer <fpauer@gmail.com>
2016-09-25 11:38:06 -0700

    404 component

--- meet-irl/app/app.js
+++ meet-irl/app/app.js
@@ -4,7 +4,8 @@
     angular.module('meetIrl', [
         'ui.router',
         'api.users',
-        'components.users'
+        'components.users',
+        'components.missingno' // add missingno component
     ])
         .config(function($urlRouterProvider) {
             $urlRouterProvider.otherwise("/users");
